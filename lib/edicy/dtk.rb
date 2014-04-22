585fde3c051c87d40dd2400154a85d9751d526f2
Mikk Pristavka <mikk@fraktal.ee>
2014-04-22 09:22:40 +0300

    Add more options to .edicy file and change variable name for host.

--- lib/edicy/dtk.rb
+++ lib/edicy/dtk.rb
@@ -7,14 +7,17 @@ module Edicy
     class << self
       def read_config(file = nil)
         config = {
-          :site_url => nil,
-          :api_token => nil
+          :host => nil,
+          :api_token => nil,
+          :editmode => false,
+          :remote => false
         }
         if !file.nil? && !file.empty? && File.exists?(File.expand_path(file))
           options = ParseConfig.new(File.expand_path(file))
-          config[:site_url] = options["OPTIONS"]["url"]
+          config[:host] = options["OPTIONS"]["host"]
           config[:api_token] = options["OPTIONS"]["api_token"]
           config[:editmode] = options["OPTIONS"]["editmode"] == "true"
+          config[:remote] = options["OPTIONS"]["remote"] == "true"
         end
         config
       end
