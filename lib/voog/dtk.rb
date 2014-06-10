872e0975ebaab4e7cf2dab5d9e02cd0517ca8ee4
Mikk Pristavka <mikk@fraktal.ee>
2014-06-10 14:15:23 +0300

    Add support for multiple sites in one configuration file.
    add --site/-s global option
    enable reading parameters by providing a block:
    `kit pull -s site2` will use the options under [site2] in the .voog file
    if no site is provided, the first one is used by default

--- lib/voog/dtk.rb
+++ lib/voog/dtk.rb
@@ -12,45 +12,56 @@ module Voog
         File.exists? CONFIG_FILENAME
       end
 
-      def read_config(file = nil)
+      def read_config(block = nil, file = CONFIG_FILENAME)
         config = {
           :host => nil,
           :api_token => nil
         }
         if !file.nil? && !file.empty? && File.exists?(File.expand_path(file))
           options = ParseConfig.new(File.expand_path(file))
-          config[:host] = options["OPTIONS"].fetch("host")
-          config[:api_token] = options["OPTIONS"].fetch("api_token")
-
-          if options["OPTIONS"].fetch("editmode", false)
-            config[:editmode] = options["OPTIONS"].fetch("editmode") == "true"
+          @block = if block.nil?
+            options.params.keys.first
+          else
+            if options.params.key?(block)
+              block
+            else
+              fail "Site '#{block}' not found in the configuration file!".red
+            end
           end
-
-          if options["OPTIONS"].fetch("remote", false)
-            config[:remote] = options["OPTIONS"].fetch("remote") == "true"
-          end
+          config[:host] = options.params[@block].fetch("host")
+          config[:api_token] = options.params[@block].fetch("api_token")
         end
         config
       end
 
-      def write_config(host, api_token, silent=false)
-        options = File.exists?(CONFIG_FILENAME) ? self.read_config(CONFIG_FILENAME) : nil
+      def write_config(host, api_token, block, silent=false)
+        unless File.exists?(CONFIG_FILENAME)
+          File.new(CONFIG_FILENAME, 'w')
+        end
 
-        File.delete CONFIG_FILENAME if File.exists? CONFIG_FILENAME
+        options = ParseConfig.new(File.expand_path(CONFIG_FILENAME))
 
-        if options
-          puts "Writing new configuration options to existing #{CONFIG_FILENAME} file.".white unless silent
-          options[:host] = host unless host.empty?
-          options[:api_token] = api_token unless api_token.empty?
+        if options.params.key?(block)
+          puts "Writing new configuration options to existing config block.".white unless silent
+          options.params[block]['host'] = host unless host.empty?
+          options.params[block]['api_token'] = api_token unless api_token.empty?
         else
-          puts "Writing configuration options to missing #{CONFIG_FILENAME} file.".white unless silent
-          options = { host: host, api_token: api_token }
+          puts "Writing configuration options to new config block.".white unless silent
+          options.params[block] = {
+            'host' => host,
+            'api_token' => api_token
+          }
         end
 
         File.open(CONFIG_FILENAME, 'w') do |file|
-          file.write("[OPTIONS]\n")
-          options.each do |key,value|
-            file.write("  #{key}=#{value}\n")
+          file.truncate(0)
+          file << "\n"
+          options.params.each do |param|
+            file << "[#{param[0]}]\n"
+            param[1].keys.each do |key|
+              file << "  #{key}=#{param[1][key]}\n"
+            end
+            file << "\n"
           end
         end
       end
@@ -68,7 +79,7 @@ module Voog
       def print_debug_info(exception)
         puts
         puts "Exception: #{exception.class}"
-        if is_api_error?(exception)
+        if is_api_error?(exception) && exception.respond_to?(:response) && exception.response
           pp exception.response
         end
         puts exception.backtrace
@@ -76,12 +87,16 @@ module Voog
 
       def handle_exception(exception, debug, notifier=nil)
         error_msg = if is_api_error?(exception)
-          if exception.respond_to?(:response) && exception.response.fetch(:headers, {}).fetch(:content_type, '') =~ /application\/json/
-            body = JSON.parse(exception.response.fetch(:body))
-            "#{body.fetch('message', '')} #{("Errors: " + body.fetch('errors', '').inspect) if body.fetch('errors', nil)}".red
+          if exception.respond_to?(:response) && exception.response
+            if exception.response.fetch(:headers, {}).fetch(:content_type, '') =~ /application\/json/
+              body = JSON.parse(exception.response.fetch(:body))
+              "#{body.fetch('message', '')} #{("Errors: " + body.fetch('errors', '').inspect) if body.fetch('errors', nil)}".red
+            else
+              exception.response.fetch(:body)
+            end + "(error code #{exception.response[:status]})".red
           else
-            exception.response.fetch(:body)
-          end + "(error code #{exception.response[:status]})".red
+            "#{exception}"
+          end
         else
           "#{exception}"
         end
@@ -94,6 +109,8 @@ module Voog
           puts error_msg
         end
         print_debug_info(exception) if debug
+      rescue => e
+        handle_exception e, debug, notifier
       end
     end
   end
--- spec/models/dtk/config_spec.rb
+++ spec/models/dtk/config_spec.rb
@@ -6,7 +6,7 @@ describe Voog::Dtk do
 
     context 'when a config file is provided' do
 
-      let(:config) { Voog::Dtk.read_config 'spec/fixtures/.voog' }
+      let(:config) { Voog::Dtk.read_config nil, 'spec/fixtures/.voog' }
 
       it 'api_token should be exactly as configured' do
         expect(config[:api_token]).to eq('afcf30182aecfc8155d390d7d4552d14')
@@ -16,11 +16,30 @@ describe Voog::Dtk do
         expect(config[:host]).to eq('voog.local')
       end
 
+      context 'when there are multiple blocks' do
+        let(:block1) { Voog::Dtk.read_config nil, 'spec/fixtures/.voog2' }
+        let(:block2) { Voog::Dtk.read_config 'testblock', 'spec/fixtures/.voog2' }
+
+        it 'should default to the first one if block is not provided' do
+          expect(block1[:host]).to eq('voog.local')
+        end
+
+        it 'should read the correct options when a block is provided' do
+          expect(block2[:api_token]).to eq('123')
+        end
+
+        it 'should fail when a non-existant block is provided' do
+          expect{
+            block3 = Voog::Dtk.read_config 'wrongname', 'spec/fixtures/.voog2'
+          }.to raise_error
+        end
+      end
+
     end
 
     context 'when the provided filename is empty' do
 
-      let(:config) { Voog::Dtk.read_config '' }
+      let(:config) { Voog::Dtk.read_config nil, '' }
 
       it 'api_token should be nil' do
         expect(config[:api_token]).to eq(nil)
@@ -34,7 +53,7 @@ describe Voog::Dtk do
 
     context 'when a filename is not provided' do
 
-      let(:config) { Voog::Dtk.read_config }
+      let(:config) { Voog::Dtk.read_config nil, nil }
 
       it 'api_token should be nil' do
         expect(config[:api_token]).to eq(nil)
@@ -48,7 +67,7 @@ describe Voog::Dtk do
 
     context 'when the provided filename is invalid' do
 
-      let(:config) { Voog::Dtk.read_config 'foo.bar' }
+      let(:config) { Voog::Dtk.read_config nil, 'foo.bar' }
 
       it 'api_token should be nil' do
         expect(config[:api_token]).to eq(nil)
