cb7ce7cd7f7d90c2710b477bb8385b10b88f064c
Mikk Pristavka <mikk@voog.com>
2016-02-19 11:46:39 +0200

    Draft Kit.actions

---
+++ src/actions.js
@@ -0,0 +1,27 @@
+var config = require('./config');
+var sites = require('./config');
+
+module.exports = {
+/*
+
+  pull :: [string] -> promise
+  pull(paths)
+
+  pullAll :: -> promise
+  pullAll()
+
+  push :: [string] -> object? -> promise
+  push(files, [{overwrite}])
+
+  pushAll :: object? -> promise
+  pushAll([{overwrite}])
+
+  add :: [string] -> promise
+  add(files)
+
+  remove :: [string] -> promise
+  remove(files)
+
+*/
+}
+
--- src/core.js
+++ src/core.js
@@ -4,10 +4,12 @@ var fs = require('fs');
 var fileUtils = require('./file_utils');
 var config = require('./config');
 var sites = require('./sites');
+var actions = require('./actions');
 
 module.exports = {
   fileUtils: fileUtils,
   config: config,
-  sites: sites
+  sites: sites,
+  actions: actions
 };
 
